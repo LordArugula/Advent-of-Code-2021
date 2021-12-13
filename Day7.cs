ea1d118605543e5cb8f94b0879c5d664759e03ae
Victor Pan <victorpan2017@gmail.com>
2021-12-12 17:02:44 -0800

    day 7

---
+++ Day7.cs
@@ -0,0 +1,60 @@
+﻿namespace AdventOfCode2021;
+
+public static class Day7
+{
+    public static void Part1()
+    {
+        string[] inputs = InputHelper.GetInput(7);
+        IEnumerable<int> crabPositions = inputs[0]
+            .Split(',')
+            .Select(x => int.Parse(x));
+
+        int bestPosition = -1;
+        int bestCost = int.MaxValue;
+
+        for (int position = 0; position < crabPositions.Max(); position++)
+        {
+            int cost = 0;
+            foreach (int crab in crabPositions)
+            {
+                cost += Math.Abs(crab - position);
+            } 
+
+            if (cost < bestCost)
+            {
+                bestCost = cost;
+                bestPosition = position;
+            }
+        }
+        Console.WriteLine(bestCost);
+    }
+
+    public static void Part2()
+    {
+
+        string[] inputs = InputHelper.GetInput(7);
+        IEnumerable<int> crabPositions = inputs[0]
+            .Split(',')
+            .Select(x => int.Parse(x));
+
+        int bestPosition = -1;
+        int bestCost = int.MaxValue;
+
+        for (int position = 0; position < crabPositions.Max(); position++)
+        {
+            int cost = 0;
+            foreach (int crab in crabPositions)
+            {
+                int distance = Math.Abs(crab - position);
+                cost += (distance * (distance + 1)) / 2;
+            }
+
+            if (cost < bestCost)
+            {
+                bestCost = cost;
+                bestPosition = position;
+            }
+        }
+        Console.WriteLine(bestCost);
+    }
+}
