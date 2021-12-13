98dd99232decdc27c90d03085937cb5a51f61a70
Victor Pan <victorpan2017@gmail.com>
2021-12-13 12:54:30 -0800

    Day 8 part 1

---
+++ Day8.cs
@@ -0,0 +1,40 @@
+﻿namespace AdventOfCode2021;
+
+public static class Day8
+{
+    public static void Part1()
+    {
+        string[] inputs = InputHelper.GetInput(8);
+
+        int count = 0;
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            ReadOnlySpan<char> inputAsSpan = inputs[i].AsSpan();
+            int delimIndex = inputAsSpan.IndexOf('|');
+            string[] outputDigits = inputAsSpan[(delimIndex + 1)..]
+                .ToString()
+                .Split(' ');
+            for (int j = 0; j < outputDigits.Length; j++)
+            {
+                if (outputDigits[j].Length == 2
+                    || outputDigits[j].Length == 3
+                    || outputDigits[j].Length == 4
+                    || outputDigits[j].Length == 7)
+                {
+                    count++;
+                }
+            }
+        }
+
+        Console.Write(count);
+    }
+
+    public static void Part2()
+    {
+        string[] inputs = InputHelper.GetInput(8);
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            NewMethod(inputs[i]);
+        }
+    }
+
