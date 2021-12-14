0801c9b509c642d55fac2413a141e7f43a48a754
Victor Pan <victorpan2017@gmail.com>
2021-12-13 18:16:38 -0800

    Day 10 part 2

--- Day10.cs
+++ Day10.cs
@@ -2,12 +2,20 @@
 
 public static class Day10
 {
-    private static readonly Dictionary<char, int> ScoreMap = new Dictionary<char, int>
+    private static readonly Dictionary<char, int> ErrorScoreMap = new Dictionary<char, int>
     {
         { ')', 3 },
         { ']', 57 },
         { '}', 1197 },
         { '>', 25137 }
+    };
+
+    private static readonly Dictionary<char, ulong> AutoCompleteScoreMap = new Dictionary<char, ulong>
+    {
+        { ')', 1 },
+        { ']', 2 },
+        { '}', 3 },
+        { '>', 4 }
     };
 
     private static readonly Dictionary<char, char> SymbolPairs = new Dictionary<char, char>
@@ -76,7 +84,7 @@ public static class Day10
                     }
                     else
                     {
-                        return ScoreMap[symbol];
+                        return ErrorScoreMap[symbol];
                     }
                 }
             }
@@ -99,5 +107,62 @@ public static class Day10
     {
         string[] inputs = InputHelper.GetInput(10);
 
+        List<ulong> autoCompleteScores = new List<ulong>();
+
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            string line = inputs[i];
+            ulong score = GetAutoCompleteScore(line);
+            if (score == 0)
+            {
+                continue;
+            }
+            autoCompleteScores.Add(score);
+        }
+
+        autoCompleteScores.Sort();
+        Console.WriteLine(autoCompleteScores[autoCompleteScores.Count / 2]);
+    }
+
+    private static ulong GetAutoCompleteScore(string line)
+    {
+        Stack<char> groups = new Stack<char>();
+
+        for (int j = 0; j < line.Length; j++)
+        {
+            char symbol = line[j];
+
+            if (IsOpenSymbol(symbol))
+            {
+                // new level of nesting
+                groups.Push(symbol);
+            }
+            else
+            {
+                // close nesting
+                if (groups.TryPeek(out char prevSymbol))
+                {
+                    if (SymbolPairs[prevSymbol] == symbol)
+                    {
+                        groups.Pop();
+                    }
+                    else
+                    {
+                        // Corrupted
+                        return 0;
+                    }
+                }
+            }
+        }
+
+        IEnumerable<ulong> scores = groups
+            .Select(symbol => AutoCompleteScoreMap[SymbolPairs[symbol]]);
+
+        ulong totalScore = 0;
+        foreach (ulong score in scores)
+        {
+            totalScore = 5u * totalScore + score;
+        }
+        return totalScore;
     }
 }
